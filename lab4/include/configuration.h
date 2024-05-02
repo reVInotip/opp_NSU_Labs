d9813419501174ee7375ba9b1ff4b0b33f8a4d45
reVInotip <grigoriy.novikov220@gmail.com>
2024-05-02 17:21:37 +0700

    a

--- lab4/include/configuration.h
+++ lab4/include/configuration.h
@@ -4,11 +4,11 @@
 #define D_y 2
 #define D_z 2
 
-#define N_x 2
-#define N_y 2
-#define N_z 2
+#define N_x 100
+#define N_y 100
+#define N_z 100
 
 #define EPSILON 1e-8
 
 #define A 1e5
-#define DELTA_MAX 0.5
+#define DELTA_MAX 100
